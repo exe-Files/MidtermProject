3477e50b97edc88200c955423798634aea939d2a
mklagassey <mklagassey@gmail.com>
2021-07-21 12:45:32 -0400

    add category to new bucket item

--- BuckIt/src/main/java/com/skilldistillery/buckit/controllers/AddBucketItemController.java
+++ BuckIt/src/main/java/com/skilldistillery/buckit/controllers/AddBucketItemController.java
@@ -1,6 +1,5 @@
 package com.skilldistillery.buckit.controllers;
 
-
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -12,13 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.buckit.dao.BucketItemDAO;
+import com.skilldistillery.buckit.dao.CategoryDAO;
 import com.skilldistillery.buckit.dao.CountryDAO;
 import com.skilldistillery.buckit.dao.LocationDAO;
 import com.skilldistillery.buckit.dao.UserBucketItemDAO;
 import com.skilldistillery.buckit.dao.UserDAO;
 import com.skilldistillery.buckit.entities.BucketItem;
+import com.skilldistillery.buckit.entities.Category;
 import com.skilldistillery.buckit.entities.Country;
 import com.skilldistillery.buckit.entities.Location;
 import com.skilldistillery.buckit.entities.Note;
@@ -32,56 +34,64 @@ public class AddBucketItemController {
 
 	@Autowired
 	private BucketItemDAO bucketDao;
-	
+
 	@Autowired
 	private UserBucketItemDAO userBucketDao;
 
 	@Autowired
 	private LocationDAO locationDao;
-	
+
 	@Autowired
 	private CountryDAO countryDao;
-	
-	@RequestMapping("newbucketitem.do")
-	public String gotoMakeNewBucketItem(Model model) {	
-		List<Country> countryList = countryDao.getAllCountries();
 
+	@Autowired
+	private CategoryDAO categoryDao;
+
+	@RequestMapping("newbucketitem.do")
+	public String gotoMakeNewBucketItem(Model model) {
+		List<Country> countryList = countryDao.getAllCountries();
 		model.addAttribute("countries", countryList);
+		List<Category> categoryList = categoryDao.getAllCategories();
+		model.addAttribute("categories", categoryList);
 
 		return "newBucketItem";
 	}
 
 	@RequestMapping(path = "newbucketinfo.do", method = RequestMethod.POST)
-	public ModelAndView createNewBucketItem(BucketItem item, @RequestParam(name = "countryId") String country, Location location, HttpSession session) {
-		ModelAndView mv = new ModelAndView();	
+	public ModelAndView createNewBucketItem(BucketItem item, @RequestParam(name = "countryId") String country,
+			@RequestParam(name = "categoryId") Integer catId, Location location, HttpSession session,
+			RedirectAttributes ra) {
+		ModelAndView mv = new ModelAndView();
 		User user = (User) session.getAttribute("loggedInUser");
 		UserBucketItem userItem = new UserBucketItem();
-		Country newCountry = new Country();
-		
+
+		if (location.getCityArea() != null && location.getCityArea() != "" || country.length() == 3) {
+			location = locationDao.createLocation(location);
+		}
+
 		if (country.length() == 3) {
-			newCountry.setCountryCode(country);
+			Country newCountry = countryDao.findByCountryCode(country);
 			location.setCountryCode(newCountry);
 		}
 		
-		
-		
-		if (location.getCityArea() != null && location.getCityArea() != "") {
-			mv.addObject("location", locationDao.createLocation(location));
-			item.setLocation(location);
+		if (catId > 0) {
+			item.addCategory(categoryDao.findCategoryById(catId));		
 		}
 		
-	
+		if (location.getId() != 0) {
+			item.setLocation(location);			
+		}
+
 		item.setCreatedByUser(user);
-		
-		mv.addObject("item", bucketDao.createBucketItem(item));
-		
-		
+		item = bucketDao.createBucketItem(item);
+
 		userItem.setBucketItem(item);
 		userItem.setUser(user);
-				
-		mv.addObject("userItem", userBucketDao.createBucketItem(userItem));
-		
-		mv.setViewName("new-item-added");		
+
+		UserBucketItem newUserItem = userBucketDao.createBucketItem(userItem);
+
+		ra.addAttribute("itemId", newUserItem.getId());
+		mv.setViewName("redirect:viewUserBucketItem.do");
 
 		return mv;
 	}
