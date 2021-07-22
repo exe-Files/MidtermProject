46d686f9afaa46663118715fc39622a84634f958
Brandon <files.brandon@gmail.com>
2021-07-22 15:30:46 -0500

    Add for war file

--- BuckIt/src/main/java/com/skilldistillery/buckit/BuckItApplication.java
+++ BuckIt/src/main/java/com/skilldistillery/buckit/BuckItApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.buckit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BuckItApplication {
+public class BuckItApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BuckItApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuckItApplication.class, args);
